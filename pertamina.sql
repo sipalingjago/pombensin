2725bf53399d95330bf052e17a18ee7d95da5130
fajar pratama <esfajarx2@gmail.com>
2020-05-19 15:06:12 +0800

    update db

--- pertamina.sql
+++ pertamina.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2020 at 08:26 AM
--- Server version: 5.6.21
--- PHP Version: 5.5.19
+-- Generation Time: May 19, 2020 at 09:05 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 5.6.39
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `pertamina`
@@ -26,24 +28,47 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-`id` int(11) NOT NULL,
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `id_lokasi_pom` int(11) NOT NULL,
+  `id_jadwal_shift` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
-  `jadwal_shift` varchar(20) NOT NULL,
   `foto` varchar(300) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `hak_akses` int(11) NOT NULL,
   `status` enum('0','1') NOT NULL DEFAULT '1'
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `nama`, `jadwal_shift`, `foto`, `email`, `password`, `hak_akses`, `status`) VALUES
-(2, 'Administrator', '', NULL, 'admin@pertaminasumbawa.com', '25ed1bcb423b0b7200f485fc5ff71c8e', 0, '1'),
-(6, 'Erwin Mardinata, S.Kom', '21.00-23.00', NULL, 'admin@sumbawakab.go.id', '25ed1bcb423b0b7200f485fc5ff71c8e', 2, '1');
+INSERT INTO `admin` (`id`, `id_lokasi_pom`, `id_jadwal_shift`, `nama`, `foto`, `email`, `password`, `hak_akses`, `status`) VALUES
+(2, 0, 0, 'Administrator', NULL, 'admin@pertaminasumbawa.com', '25ed1bcb423b0b7200f485fc5ff71c8e', 0, '1'),
+(6, 2, 1, 'Erwin Mardinata, S.Kom', NULL, 'admin@sumbawakab.go.id', '25ed1bcb423b0b7200f485fc5ff71c8e', 2, '1'),
+(7, 2, 2, 'ahmad', NULL, 'ahmad@gmail.com', '25ed1bcb423b0b7200f485fc5ff71c8e', 2, '1'),
+(8, 2, 3, 'fajar', NULL, 'fajar@gmail.com', '4124bc0a9335c27f086f24ba207a4912', 2, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal_shift`
+--
+
+CREATE TABLE `jadwal_shift` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jadwal_shift`
+--
+
+INSERT INTO `jadwal_shift` (`id`, `nama`) VALUES
+(1, '08.00-15.00'),
+(2, '15.00-21.00'),
+(3, '21.00-08.00');
 
 -- --------------------------------------------------------
 
@@ -51,10 +76,20 @@ INSERT INTO `admin` (`id`, `nama`, `jadwal_shift`, `foto`, `email`, `password`, 
 -- Table structure for table `jenis_bbm`
 --
 
-CREATE TABLE IF NOT EXISTS `jenis_bbm` (
-`id` int(11) NOT NULL,
+CREATE TABLE `jenis_bbm` (
+  `id` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jenis_bbm`
+--
+
+INSERT INTO `jenis_bbm` (`id`, `nama`) VALUES
+(2, 'Premium'),
+(3, 'Pertalite'),
+(4, 'Pertamax'),
+(5, 'Solar');
 
 -- --------------------------------------------------------
 
@@ -62,14 +97,14 @@ CREATE TABLE IF NOT EXISTS `jenis_bbm` (
 -- Table structure for table `lokasi_pom`
 --
 
-CREATE TABLE IF NOT EXISTS `lokasi_pom` (
-`id` int(11) NOT NULL,
+CREATE TABLE `lokasi_pom` (
+  `id` int(11) NOT NULL,
   `nama` varchar(250) NOT NULL,
   `alamat` text NOT NULL,
   `kontak` varchar(20) NOT NULL,
   `lat` varchar(50) NOT NULL,
   `long` varchar(50) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `lokasi_pom`
@@ -84,14 +119,23 @@ INSERT INTO `lokasi_pom` (`id`, `nama`, `alamat`, `kontak`, `lat`, `long`) VALUE
 -- Table structure for table `nozzel`
 --
 
-CREATE TABLE IF NOT EXISTS `nozzel` (
-`id` int(11) NOT NULL,
+CREATE TABLE `nozzel` (
+  `id` int(11) NOT NULL,
   `id_tangki` int(11) NOT NULL,
-  `id_admin` int(11) NOT NULL,
+  `id_jadwal_shift` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `teller_akhir` int(11) NOT NULL,
-  `teller_awal` int(11) NOT NULL
+  `teller_awal` int(11) NOT NULL,
+  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nozzel`
+--
+
+INSERT INTO `nozzel` (`id`, `id_tangki`, `id_jadwal_shift`, `nama`, `teller_akhir`, `teller_awal`, `tanggal`) VALUES
+(3, 3, 3, 'nozzel 1', 500000, 1000000, '2020-05-19 14:44:47'),
+(4, 3, 2, 'nozzel 2', 909128, 1029310, '2020-05-19 14:54:24');
 
 -- --------------------------------------------------------
 
@@ -99,11 +143,18 @@ CREATE TABLE IF NOT EXISTS `nozzel` (
 -- Table structure for table `tangki`
 --
 
-CREATE TABLE IF NOT EXISTS `tangki` (
-`id` int(11) NOT NULL,
+CREATE TABLE `tangki` (
+  `id` int(11) NOT NULL,
   `id_jenis_bbm` int(11) NOT NULL,
   `nama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tangki`
+--
+
+INSERT INTO `tangki` (`id`, `id_jenis_bbm`, `nama`) VALUES
+(3, 4, 'pertamax 1');
 
 --
 -- Indexes for dumped tables
@@ -113,31 +164,37 @@ CREATE TABLE IF NOT EXISTS `tangki` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `jadwal_shift`
+--
+ALTER TABLE `jadwal_shift`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `jenis_bbm`
 --
 ALTER TABLE `jenis_bbm`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `lokasi_pom`
 --
 ALTER TABLE `lokasi_pom`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `nozzel`
 --
 ALTER TABLE `nozzel`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tangki`
 --
 ALTER TABLE `tangki`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -147,27 +204,39 @@ ALTER TABLE `tangki`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `jadwal_shift`
+--
+ALTER TABLE `jadwal_shift`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
 -- AUTO_INCREMENT for table `jenis_bbm`
 --
 ALTER TABLE `jenis_bbm`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `lokasi_pom`
 --
 ALTER TABLE `lokasi_pom`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `nozzel`
 --
 ALTER TABLE `nozzel`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `tangki`
 --
 ALTER TABLE `tangki`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
